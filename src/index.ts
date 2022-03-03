984973717dcea2668f04133c238539a15bb0fc59
Sanuja Methmal <methmal66@yahoo.com>
2022-03-03 21:06:09 +0530

    init express server

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import Subscriber from './db'
 
 const PORT = process.env.PORT || 8000
 const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 const app = express()
 
-app.set('view engine', 'ejs')
-
-app.get('/subscribe', (req, res) => {
-   res.status(200).render('subscribe')
-})
-
-app.post('/api/subscribe', urlEncodedParser, (req, res) => {
-   const doc = new Subscriber({
-      email: req.body.email,
-      frequency: req.body.frequency,
-   })
-   doc.save((error) => {
-      if (error) return res.status(500).send(error)
-      res.status(200).send('New subscriber stored successfully!')
+app.get('/api/sticker/text', urlEncodedParser, (req, res) => {
+   return res.json({
+      text: req.query.text,
+      fontColor: req.query.fontColor,
+      backgroundColor: req.query.backgroundColor,
    })
 })
 
